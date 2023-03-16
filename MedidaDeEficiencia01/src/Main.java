f7b821d68831d8f84e57d999d8c18ebb19fe96b6
Maria de Fátima Nunes Alves <94319702+alvesmariadefatima@users.noreply.github.com>
2023-03-16 17:00:12 -0300

    Update Main.java

--- MedidaDeEficiencia01/src/Main.java
+++ MedidaDeEficiencia01/src/Main.java
@@ -1,7 +1,15 @@
 class Main {
-    public static void main(String[] args) {
-            Fila fila1 = new Fila();
-            System.out.print(fila1.toString());
+  public static void main(String[] args) {
+    Fila fila = new Fila(3);
 
-        }
-    }
+    // set valores para primeiro, ultimo e atual
+    fila.setPrimeiro("Joana");
+    fila.setUltimo("Marcela");
+    fila.setAtual("Juliana");
+
+    // imprime os valores usando os métodos get
+    System.out.println("Primeiro da fila de espera do banco: " + fila.getPrimeiro());
+    System.out.println("Último da fila de espera do banco: " + fila.getUltimo());
+    System.out.println("Atual da fila de espera do banco: " + fila.getAtual());
+  }
+}
