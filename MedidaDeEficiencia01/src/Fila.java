9d596c12f71a7fdc415899cb770a5135e6dea821
Maria de Fátima Nunes Alves <94319702+alvesmariadefatima@users.noreply.github.com>
2023-03-17 08:03:43 -0300

    Update Fila.java

--- MedidaDeEficiencia01/src/Fila.java
+++ MedidaDeEficiencia01/src/Fila.java
@@ -2,107 +2,107 @@ import java.util.Queue;
 
 public class Fila {
 
-  private Queue<String> fila;
-  private Object[] vetor;
-  private String primeiro, ultimo, atual;
-  private int tamanho;
+    private Queue<String> fila;
+    private Object[] vetor;
+    private String primeiro, ultimo, atual;
+    private int tamanho;
 
-  Fila(int tamanho) {
-    this.primeiro = "";
-    this.ultimo = "";
-    this.tamanho = tamanho;
-    this.atual = atual;
-    this.vetor = new Object[tamanho];
-  }
-
-  boolean estaVazio() {
-    if (this.primeiro.equals("")) {
-      return true;
-    } else {
-      return false;
+    Fila(int tamanho) {
+        this.primeiro = "";
+        this.ultimo = "";
+        this.tamanho = tamanho;
+        this.vetor = new Object[tamanho];
     }
-  }
 
-  boolean estaCheio() {
-    if (this.ultimo.equals("")) {
-      return true;
-    } else {
-      return false;
+    boolean estaVazio() {
+        if (this.primeiro.equals("")) {
+            return false;
+        } else {
+            return true;
+        }
     }
-  }
 
-  void enfileira(Object elemento) {
-    if (estaCheio()) {
-      System.out.println("Fila Cheia!!");
-    } else {
-      this.vetor[Integer.parseInt(ultimo)] = elemento;
-      System.out.println("Elemento Inserido!");
+    boolean estaCheio() {
+        if (this.ultimo.equals("")) {
+            return false;
+        } else {
+            return true;
+        }
     }
-  }
 
-  void desenfileirar() {
-    if (estaVazio()) {
-      System.out.println("Fila Vazia!!");
-    } else {
-      this.vetor[Integer.parseInt(primeiro)] = "";
-      this.primeiro = "";
-      System.out.println("Elemento removido!");
+    void enfileira(Object elemento) {
+        if (estaCheio()) {
+            System.out.println("Fila Cheia!!");
+        } else {
+            this.vetor[Integer.parseInt(ultimo)] = elemento;
+            System.out.println("Elemento Inserido!");
+        }
     }
-  }
 
-  void imprimir() {
-
-    for (int i = 0; i < this.tamanho; i++) {
-      System.out.println(this.vetor[i]);
+    void desenfileirar() {
+        if (estaVazio()) {
+            System.out.println("Fila Vazia!!");
+        } else {
+            this.vetor[Integer.parseInt(primeiro)] = "";
+            this.primeiro = "";
+            System.out.println("Elemento removido!");
+        }
     }
-  }
 
-  @Override
-  public String toString() {
-    return "Primeiro da Fila: " + primeiro + "Último da Fila: " + ultimo + "Atual da Fila: " + atual;
-  }
+    void imprimir() {
 
-  public void primeiroFila() {
-    System.out.println(primeiro);
-  }
+        for (int i = 0; i < this.tamanho; i++) {
+            System.out.println(this.vetor[i]);
+        }
+    }
 
-  public void ultimoFila() {
-    System.out.println(ultimo);
-  }
+    @Override
+    public String toString() {
+        return "Primeiro da fila de espera do banco: " + primeiro + "Último da Fila de espera do banco: " + ultimo + "Atual da Fila de espera do banco: " + atual;
+    }
 
-  public void atualFila() {
-    System.out.println(atual);
-  }
+    public void primeiroFila() {
+        System.out.println(primeiro);
+    }
 
-  public String getPrimeiro() {
-    return primeiro;
-  }
+    public void ultimoFila() {
+        System.out.println(ultimo);
+    }
 
-  public void setPrimeiro(String primeiro) {
-    this.primeiro = primeiro;
-  }
+    public void atualFila() {
+        System.out.println(atual);
+    }
 
-  public String getUltimo() {
-    return ultimo;
-  }
+    public String getPrimeiro() {
+        return primeiro;
+    }
 
-  public void setUltimo(String ultimo) {
-    this.ultimo = ultimo;
-  }
+    public void setPrimeiro(String primeiro) {
+        this.primeiro = primeiro;
+    }
 
-  public String getAtual() {
-    return atual;
-  }
+    public String getUltimo() {
+        return ultimo;
+    }
 
-  public void setAtual(String atual) {
-    this.atual = atual;
-  }
+    public void setUltimo(String ultimo) {
+        this.ultimo = ultimo;
+    }
 
-  public Object[] getVetor() {
-    return vetor;
-  }
+    public String getAtual() {
+        return atual;
+    }
 
-  public void setVetor(Object[] vetor) {
-    this.vetor = vetor;
-  }
+    public void setAtual(String atual) {
+        this.atual = atual;
+    }
+
+    public Object[] getVetor() {
+
+        return vetor;
+    }
+
+    public void setVetor(Object[] vetor) {
+        this.vetor = vetor;
+    }
 }
